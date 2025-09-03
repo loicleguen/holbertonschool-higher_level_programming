bdc73270927ac3e673e50bb4a0099096c90c9402
loicleguen <loic.leguen@hotmail.fr>
2025-09-03 11:07:51 +0200

    add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+
+    if num_args == 1:
+        print("{} argument:".format(num_args))
+    elif num_args == 0:
+        print("{} arguments.".format(num_args))
+    else:
+        print("{} arguments:".format(num_args))
+    if num_args > 0:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
