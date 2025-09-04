713ef8367e5024a2c3a4592bee763d3e8b498022
loicleguen <loic.leguen@hotmail.fr>
2025-09-04 15:17:47 +0200

    fix: correct variable name 'orperator' in output formatting

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print(f"{a} {op} {b} = {result}")
+    print("{} {} {} = {}".format(a, orperator, b, result))
