fe05fe7c9f1fd3abe181f2cf1bb51886027bb699
loicleguen <loic.leguen@hotmail.fr>
2025-09-15 10:43:29 +0200

    docs: add Square class definition in 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
