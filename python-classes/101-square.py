52b38d3e5199cc0e29e4f21069fbe07e2b166022
loicleguen <loic.leguen@hotmail.fr>
2025-09-15 14:05:50 +0200

    feat: add Square class with size and area methods

---
+++ python-classes/101-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Module defining a Square with size and area
+"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        print(self.__str__(), end="")
+
+    def __str__(self):
+        if self.size == 0:
+            return "\n"
+        lines = [""] * self.position[1]
+        for _ in range(self.size):
+            lines.append(" " * self.position[0] + "#" * self.size)
+        return "\n".join(lines)
