6da81f6233d0389d13b0b45bc08ee44a91fdd009
loicleguen <loic.leguen@hotmail.fr>
2025-09-15 10:50:03 +0200

    feat: implement Square class with size validation and area calculation

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module defining a Square with size and area
+"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
