c3e88bb60a840441d7e1d9d61f4a215d10a8c818
loicleguen <loic.leguen@hotmail.fr>
2025-09-19 18:01:55 +0200

    feat: implement BaseGeometry class with area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class representing basic geometric shapes."""
+    def area(self):
+        raise Exception("area() is not implemented")
