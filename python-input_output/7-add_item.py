9c0c511112bde01355631c87770abe6d4f96c160
loicleguen <loic.leguen@hotmail.fr>
2025-09-29 11:45:34 +0200

    feat: add 7-add_item.py to manage command line arguments and save them to a JSON file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+from pathlib import Path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+if Path(filename).exists():
+    items = load_from_json_file(filename)
+else:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
