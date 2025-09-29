e180905504bc9f5f9c2e76f9109f8e6024ba9105
loicleguen <loic.leguen@hotmail.fr>
2025-09-29 13:36:34 +0200

    feat: add Student class with method to convert attributes to dictionary

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Student class with a method to
+convert its nameibutes to a dictionary."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance."""
+        if not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            new_dict = {}
+            for name in attrs:
+                if name in self.__dict__:
+                    new_dict[name] = self.__dict__[name]
+            return new_dict
