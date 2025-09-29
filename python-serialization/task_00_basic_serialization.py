0c4c36773eed4b901dca29c784b4d3f869aae352
loicleguen <loic.leguen@hotmail.fr>
2025-09-29 16:27:40 +0200

    fix: update file permissions and improve docstrings for serialization functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Module for serializing and deserializing
+Python objects to/from JSON files."""
+import json
+
+
 def serialize_and_save_to_file(data, filename):
-    
+    """Serialize a Python object to a JSON file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
 def load_and_deserialize(filename):
+    """Load and deserialize a Python object from a JSON file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
