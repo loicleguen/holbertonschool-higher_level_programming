9282cef5e669994760986dc45de384e1ba615feb
loicleguen <loic.leguen@hotmail.fr>
2025-10-09 13:23:58 +0200

    refactor: update unauthorized response format in basic authentication

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -35,7 +35,7 @@ def verify_password(username, password):
 
 @auth.error_handler
 def unauthorized():
-    return jsonify({"error": "Unauthorized"}), 401
+    return "Unauthorized Access", 401
 
 
 @app.route("/basic-protected")
