293c56f05ef8658f544a86eabc65ca8a82e17f94
loicleguen <loic.leguen@hotmail.fr>
2025-10-20 15:04:01 +0200

    feat: remove CHARACTER SET and COLLATE from name column modification in first_table

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -11,5 +11,3 @@ COLLATE utf8mb4_unicode_ci;
 -- Convert the column 'name' in first_table to UTF8
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
