426ef0b14fc96fa54a4e0921eca301a16f04d46e
loicleguen <loic.leguen@hotmail.fr>
2025-10-20 15:04:39 +0200

    feat: add USE statement for database context in UTF8 conversion script

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -3,6 +3,8 @@ ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
+USE hbtn_0c_0
+
 -- Convert the table first_table to UTF8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
