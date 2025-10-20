356b10a30d7fa5925be5c0648a5bf7dbb16c03e6
loicleguen <loic.leguen@hotmail.fr>
2025-10-20 15:01:09 +0200

    feat: add SQL script to convert database and table to UTF8 (utf8mb4) encoding

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Convert the database hbtn_0c_0 to UTF8 (utf8mb4) and collation utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Convert the column 'name' in first_table to UTF8
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
