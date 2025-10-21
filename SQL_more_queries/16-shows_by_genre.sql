37b27846795a83b1aa738bf93598fb4e8d7bdc81
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 15:17:58 +0200

    fix: correct LEFT JOIN clause for genre_id reference in shows by genre query

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
