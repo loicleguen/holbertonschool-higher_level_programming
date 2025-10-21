86f44c9974bbdf7707e34afe383a2d45d2cbd6a1
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 14:55:00 +0200

    feat: add query to list all comedy shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+SELECT tv_shows.tiltle
+FROM tv_shows
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
