1ea1388024d737bf24ce488c8dded1e60ed03a88
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 14:56:20 +0200

    fix: correct typo in column name for comedy shows query

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- 15. Only Comedy
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows
 
-SELECT tv_shows.tiltle
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
