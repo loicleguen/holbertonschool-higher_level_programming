9f9be70b7553b6fdb20f22b60c0e1cedb5a99fbf
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 15:13:21 +0200

    fix: correct FROM clause in shows by genre query

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows and all genres linked to that show
 
 SELECT tv_shows.title AND tv_genres.name
-FROM tv_shows
+FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
