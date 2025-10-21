c92843d2c07930d4ac9c2b9c2b0adc4987784646
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 15:17:21 +0200

    fix: correct reference to tv_shows in LEFT JOIN clause

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
