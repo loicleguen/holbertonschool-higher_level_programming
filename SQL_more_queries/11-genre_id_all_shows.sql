121fe3e1fd9c2b1f4a0185c5eb72ddf015593231
loicleguen <loic.leguen@hotmail.fr>
2025-10-21 14:01:52 +0200

    feat: add script to list all shows with their genre IDs, including those without a genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- 11. Genre ID for all shows
+-- Script that lists all shows, including those without a genre (NULL for missing genres)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
