ae7deada7bd572f8a82928bc5f64e7e4b3a3e599
loicleguen <loic.leguen@hotmail.fr>
2025-10-23 14:45:02 +0200

    feat: add script and SQL file to display cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get MySQL credentials and database name from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+                """)
+
+    # Fetch and display all results
+    for row in cur.fetchall():
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
