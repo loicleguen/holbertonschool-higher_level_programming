d9d41eef9745166288b0c33b1f5568caf21b17f1
loicleguen <loic.leguen@hotmail.fr>
2025-10-23 09:51:35 +0200

    feat: add script to list all states with a name starting with N

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get MySQL credentials and database name from command-line arguments
+    username, password, database, state = sys.argv[1],
+    sys.argv[2], sys.argv[3], sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        state=state,
+        charset="utf8"
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name = '{}'.format(state)"
+        "ORDER BY id ASC")
+
+    # Fetch and display all results
+    for row in cur.fetchall():
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
