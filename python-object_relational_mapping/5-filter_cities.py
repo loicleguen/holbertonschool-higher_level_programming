56b21407a0f5f7864d5e7e4dc26cadc2ec06c6a5
loicleguen <loic.leguen@hotmail.fr>
2025-10-23 15:21:25 +0200

    fix: change SQL query to order cities by name instead of id

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
-        ORDER BY cities.id ASC
+        ORDER BY cities.name ASC
     """
     cur.execute(query, (state_name,))
 
