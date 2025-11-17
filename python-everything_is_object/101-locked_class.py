209df3383b878605494630ae0adf0f92852a020e
loicleguen <loic.leguen@hotmail.fr>
2025-11-17 11:43:47 +0100

    fix: add blank lines for improved readability in LockedClass

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 class LockedClass:
     """
     Prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name
